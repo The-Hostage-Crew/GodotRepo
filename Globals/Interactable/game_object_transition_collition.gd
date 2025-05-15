cf025df29e9ddcaea2c4d55d1eec7c00f2a7bcd4
murifq <muhamad.rifqi510@gmail.com>
2025-05-15 19:40:35 +0700

    feat: add collision for stage 1 and window trauma interaction with close and open the curtain

--- Globals/Interactable/game_object_transition_collition.gd
+++ Globals/Interactable/game_object_transition_collition.gd
@@ -1,10 +1,31 @@
 extends Interactable
 
-@export var scene : PackedScene
+@export var modal: Node
+@export var trauma_viewport: VideoStreamPlayer
+var showed := false
 
-func _ready() -> void:
-	pass
+func interact() -> void:
+	if modal:
+		var is_tirai: bool = modal.name == "tiraitertutup"
 
-func interact():
-	print("Changing to scene ", scene.to_string())
-	SceneTransition.change_scene(scene)
+		if is_tirai:
+			modal.set_visible(true)
+			showed = true
+			trauma_viewport.stream = load("res://assets/TheHostage/2D/constraint_[trauma]/ConstraintTraumaEnemyAngry.ogv")
+			trauma_viewport.play()
+			await get_tree().create_timer(1.5).timeout
+			modal.set_visible(false)
+			showed = false
+			return
+
+		# Logika untuk selain tirai
+		if !showed:
+			modal.set_visible(true)
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			showed = true
+		else:
+			modal.set_visible(false)
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			showed = false
+	else:
+		print("MODAL NOT FOUND")
