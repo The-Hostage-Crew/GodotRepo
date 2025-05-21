963110484e2ad0ee2b58c717dd80d61a8a04825a
Yosua Chrial Martono <yosua.chrial@ui.ac.id>
2025-05-21 23:37:31 +0700

    [FEAT] Add lock interaction in stage 2

--- Globals/Interactable/game_object_transition_collition.gd
+++ Globals/Interactable/game_object_transition_collition.gd
@@ -5,6 +5,9 @@ extends Interactable
 
 @onready var player: CharacterBody3D = %Player
 
+# Nullable use if player not found
+@export var player_ref: CharacterBody3D
+
 var showed := false
 
 
@@ -24,12 +27,20 @@ func interact() -> void:
 
 		# Logika untuk selain tirai
 		if !showed:
-			player.set_movement_enabled(false)
+			if player_ref:
+				player_ref.set_movement_enabled(false)
+			else:
+				player.set_movement_enabled(false)
+				
 			modal.set_visible(true)
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 			showed = true
 		else:
-			player.set_movement_enabled(true)
+			if player_ref:
+				player_ref.set_movement_enabled(true)
+			else:
+				player.set_movement_enabled(true)
+				
 			modal.set_visible(false)
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 			showed = false
