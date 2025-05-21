4f97886fba1020bf80c4d93ffa58ba0d701ed197
Yosua Chrial Martono <yosua.chrial@ui.ac.id>
2025-05-22 01:58:44 +0700

    [FEAT] Add new code to stage 2 lockpad

--- TheHostage/Puzzles/Door/base.gd
+++ TheHostage/Puzzles/Door/base.gd
@@ -2,16 +2,16 @@ extends Control
 
 var input_text := ""
 const MAX_LENGTH := 8
-const CORRECT_CODE := "80732243"
+@export var correct_code := "80732243"
 
 func _ready():
-	for child in $GridContainer.get_children():
+	for child in $Base/GridContainer.get_children():
 		if child is Button:
 			var button_name := child.name
 			child.pressed.connect(func(): _on_button_pressed(button_name))
 
 func _on_button_pressed(button_name: String) -> void:
-	$ClickAudio.play()
+	$Base/ClickAudio.play()
 	match button_name:
 		"Delete":
 			if input_text.length() > 0:
@@ -28,37 +28,37 @@ func _on_button_pressed(button_name: String) -> void:
 				else:
 					input_text += button_name
 
-	$Label.text = input_text
+	$Base/Label.text = input_text
 
 # Show glowing first, then resolve result
 func handle_code_check() -> void:
-	if input_text == CORRECT_CODE:
-		$LampBase.visible = true
-		$GlowingLampTrue.visible = true
+	if input_text == correct_code:
+		$Base/LampBase.visible = true
+		$Base/GlowingLampTrue.visible = true
 		await get_tree().create_timer(1.0).timeout 
 		show_correct_lamp()
 		await get_tree().create_timer(1.0).timeout 
 		Fade._in()
-		$SuccessAudio.play()
+		$Base/SuccessAudio.play()
 	else:
-		$LampBase.visible = true
-		$GlowingLampFalse.visible = true
+		$Base/LampBase.visible = true
+		$Base/GlowingLampFalse.visible = true
 		await get_tree().create_timer(1.0).timeout 
 		show_wrong_lamp()
-		$FailAudio.play()
+		$Base/FailAudio.play()
 
 	input_text = ""  
-	$Label.text = input_text 
+	$Base/Label.text = input_text 
 
 
 func show_correct_lamp():
-	$GlowingLampTrue.visible = false
-	$GlowingLampFalse.visible = false
-	$LampBase.visible = false
-	$LampBaseTrue.visible = true
+	$Base/GlowingLampTrue.visible = false
+	$Base/GlowingLampFalse.visible = false
+	$Base/LampBase.visible = false
+	$Base/LampBaseTrue.visible = true
 
 func show_wrong_lamp():
-	$GlowingLampTrue.visible = false
-	$GlowingLampFalse.visible = false
-	$LampBase.visible = true
-	$LampBaseTrue.visible = false
+	$Base/GlowingLampTrue.visible = false
+	$Base/GlowingLampFalse.visible = false
+	$Base/LampBase.visible = true
+	$Base/LampBaseTrue.visible = false
