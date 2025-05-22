48283f57f000dd12a6767142f93a03f4852e96f2
murifq <muhamad.rifqi510@gmail.com>
2025-05-22 12:32:22 +0700

    [FIX] Temp fix for exhib 1

--- TheHostage/lose_screen.gd
+++ TheHostage/lose_screen.gd
@@ -3,7 +3,7 @@ extends TextureRect
 
 func _ready() -> void:
 	SanitySystem.reset_sanity()
-	HpSystem.reset_sanity()
+	HpSystem.reset_hp()
 
 func _on_button_pressed() -> void:
 	await get_tree().create_timer(1.0).timeout
