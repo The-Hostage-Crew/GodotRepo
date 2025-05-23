6623eb8867b3fed6f0863aaf3232896855e04568
kireeeenn <87308096+kireeeenn@users.noreply.github.com>
2025-05-23 21:26:21 +0700

    Update main.gd

--- TheHostage/STAGE 1/main.gd
+++ TheHostage/STAGE 1/main.gd
@@ -6,5 +6,9 @@ func _ready() -> void:
 	if player:
 		player.set_movement_enabled(false)
 		await get_tree().create_timer(3.0).timeout
-		Notify.show_notification("Uhh.. Where am I?")
+		Notify.show_notification("You: Where am I....")
+		Notify.show_notification("Daniel: Once again, I thank you for choosing and assisting me in my escape.")
+		Notify.show_notification("Daniel: I am Daniel.")
+		Notify.show_notification("Daniel: Unfortunately, I don't even know where we are too, or how do I get here.")
+		Notify.show_notification("Daniel: But I hope you will still help me free from this trap, even though you're not here with me.")
 		player.set_movement_enabled(true)
